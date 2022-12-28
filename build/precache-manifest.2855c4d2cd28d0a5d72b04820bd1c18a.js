591d073c8c0e6ef5f1ceed6420185c065bfd1a0f
shraddha-kaparwan <110086785+shraddha-kaparwan@users.noreply.github.com>
2022-12-28 10:22:32 -0700

    adding build

---
+++ build/precache-manifest.2855c4d2cd28d0a5d72b04820bd1c18a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86426a99b25470b84afe6c806bc52604",
+    "url": "/sk-portfolio/index.html"
+  },
+  {
+    "revision": "62fe6cf79722e35a9d1a",
+    "url": "/sk-portfolio/static/css/main.f09664eb.chunk.css"
+  },
+  {
+    "revision": "0890a719274a191f2de0",
+    "url": "/sk-portfolio/static/js/2.04253ee5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sk-portfolio/static/js/2.04253ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62fe6cf79722e35a9d1a",
+    "url": "/sk-portfolio/static/js/main.17032b05.chunk.js"
+  },
+  {
+    "revision": "e321752624aad6ee9126",
+    "url": "/sk-portfolio/static/js/runtime-main.dc3d7661.js"
+  },
+  {
+    "revision": "ea82f1f9b3b6355a2777ebdaf74f1d8e",
+    "url": "/sk-portfolio/static/media/always-balanced-demo.ea82f1f9.mp4"
+  },
+  {
+    "revision": "27de698a0ff56c38eafc50df2926e5f1",
+    "url": "/sk-portfolio/static/media/always-balanced-screenshot.27de698a.png"
+  },
+  {
+    "revision": "00b368c2c785ad87470abc0fd883c424",
+    "url": "/sk-portfolio/static/media/bodega-bay.00b368c2.jpeg"
+  },
+  {
+    "revision": "286ccc456ea700a27549a17b955f87e2",
+    "url": "/sk-portfolio/static/media/education.286ccc45.png"
+  },
+  {
+    "revision": "6a6b41533d13e5cd13747801ef254f6b",
+    "url": "/sk-portfolio/static/media/france.6a6b4153.jpeg"
+  },
+  {
+    "revision": "3badb2b507b69d85eb77380f36419edc",
+    "url": "/sk-portfolio/static/media/fruit-quest-demo.3badb2b5.mp4"
+  },
+  {
+    "revision": "60c77388434832648ac76d4226971b7e",
+    "url": "/sk-portfolio/static/media/fruit-quest-screenshot.60c77388.png"
+  },
+  {
+    "revision": "c29a02d74988d10373d4d898d7fb5780",
+    "url": "/sk-portfolio/static/media/gandr-demo.c29a02d7.mp4"
+  },
+  {
+    "revision": "9b6b690d25ffc64e0861aa814b69a439",
+    "url": "/sk-portfolio/static/media/gandr-screenshot.9b6b690d.png"
+  },
+  {
+    "revision": "4980125727f8ea8ba7a869743b4b1f8e",
+    "url": "/sk-portfolio/static/media/ghent.49801257.jpeg"
+  },
+  {
+    "revision": "19b42e93da2fcea8f8584dc2df8b47c7",
+    "url": "/sk-portfolio/static/media/github-icon.19b42e93.svg"
+  },
+  {
+    "revision": "02924b30c99fbdde100f7a6af8ced6f0",
+    "url": "/sk-portfolio/static/media/golden-gate-bridge.02924b30.jpeg"
+  },
+  {
+    "revision": "02955cb167f0f70eb6848b73acb9aa36",
+    "url": "/sk-portfolio/static/media/linkedin-icon.02955cb1.svg"
+  },
+  {
+    "revision": "d252b2d1a25f883ff11b12268ac15707",
+    "url": "/sk-portfolio/static/media/new-york.d252b2d1.jpeg"
+  },
+  {
+    "revision": "4324b4e34842babcfc21fbb14f3731aa",
+    "url": "/sk-portfolio/static/media/nfph-demo.4324b4e3.mp4"
+  },
+  {
+    "revision": "94825d7f6797bb57b80e8d0d9fdca635",
+    "url": "/sk-portfolio/static/media/nfph-screenshot.94825d7f.png"
+  },
+  {
+    "revision": "72efe3b447d1f8837cae3a4e64d706bd",
+    "url": "/sk-portfolio/static/media/pavlova.72efe3b4.jpeg"
+  },
+  {
+    "revision": "ce4838ded90500bbf6cd466b1df45209",
+    "url": "/sk-portfolio/static/media/send.ce4838de.png"
+  },
+  {
+    "revision": "f31429e4b16a47024347ca7b993142d7",
+    "url": "/sk-portfolio/static/media/sk-profile.f31429e4.png"
+  },
+  {
+    "revision": "f82dc146553aa62664e9e2dec2e4aa10",
+    "url": "/sk-portfolio/static/media/skills.f82dc146.png"
+  },
+  {
+    "revision": "727f3c7e9da4bf27d0907fa436568d06",
+    "url": "/sk-portfolio/static/media/tahoe.727f3c7e.jpeg"
+  },
+  {
+    "revision": "f29c62c04e56b2884e2f2d49c722be66",
+    "url": "/sk-portfolio/static/media/volunteer-experience.f29c62c0.png"
+  }
+]);
